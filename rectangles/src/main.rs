8221bd47d784a0831e9b14604398505c7a63d6ec
Emanuel F. Oliveira <emfolg@gmail.com>
2024-11-04 03:02:34 -0300

    Rectangles: Adding support for associated functions

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -3,6 +3,7 @@ struct Rect {
     height: f32,
 }
 
+// Methods
 impl Rect {
     fn area(&self) -> f32 {
         self.width * self.height
@@ -19,19 +20,30 @@ impl Rect {
     }
 }
 
+// Associated Functions
+impl Rect {
+    fn from(width: f32, height: f32) -> Self {
+        Self { width, height }
+    }
+    fn square(side: f32) -> Rect {
+        Self::from(side, side)
+    }
+}
+
 fn main() {
     let (width, height, increase): (f32, f32, f32) = (4.0, 3.0, 0.25);
-    let mut rect = Rect { width, height };
-    let other_rects: [Rect; 2] = [
-        Rect { width: 1.5, height: 2.1 },
-        Rect { width: 8.0, height: 5.2 },
+    let mut rect = Rect::from(width, height);
+    let other_rects: [Rect; 3] = [
+        Rect::from(1.5, 2.1),
+        Rect::from(8.0, 5.2),
+        Rect::square(2.0),
     ];
     rect.print();
-    println!("Increasing rectangle by {}%...", increase * 100.0);
+    println!("Increasing main rectangle by {}%...", increase * 100.0);
     rect.increase(increase);
     rect.print();
     for (i, e) in other_rects.iter().enumerate() {
-        println!("Rectangle #{}", i);
+        println!("Rectangle #{}", i + 1);
         e.print();
         println!("Fits in main? {}", rect.can_hold(e));
     }
